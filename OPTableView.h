e4f5e292813e7155fa0f8c02445a538fa0be6758
Brandon Williams <brandon@fewbutripe.com>
2012-05-08 12:27:13 -0700

    initial commit

---
+++ OPTableView.h
@@ -0,0 +1,49 @@
+// OPTableView
+//
+// Copyright (c) 2012 Brandon Williams (http://www.opetopic.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <UIKit/UIKit.h>
+
+@class OPTableView;
+@protocol OPTableViewDelegate <UITableViewDelegate>
+@optional
+
+/**
+ Delegate methods called just before and right after the table view has snapped to an index path.
+ */
+-(void) tableView:(OPTableView*)tableView willSnapToIndexPath:(NSIndexPath*)indexPath;
+-(void) tableView:(OPTableView*)tableView didSnapToIndexPath:(NSIndexPath*)indexPath;
+
+/**
+ This delegate methods passes you the index path it is about to snap to, and you return the index path
+ it should snap to. If you don't implement this method it will snap to the default index path. If you 
+ return nil then the table view will scroll freely.
+ */
+-(NSIndexPath*) tableView:(UITableView *)tableView shouldSnapToIndexPath:(NSIndexPath*)indexPath;
+@end
+
+@interface OPTableView : UITableView
+
+@property (nonatomic, assign) id<OPTableViewDelegate> delegate;
+@property (nonatomic, assign, getter = isHorizontal) BOOL horizontal;
+@property (nonatomic, assign) BOOL snapToRows;
+
+@end
---
+++ README.md
@@ -0,0 +1,8 @@
+OPTableView
+===========
+
+
+
+## License
+
+OPTableView is available under the MIT license. See the LICENSE file for more info.
